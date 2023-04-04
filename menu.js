6f37b1c969aff2fa4e437ccaffb5833ca6953237
00live <128560704+00live@users.noreply.github.com>
2023-04-03 23:16:38 -0300

    .

--- menu.js
+++ menu.js
@@ -28,9 +28,8 @@
     })
 
     
+    
 
-    var section_fem = document.getElementById('section_fem')
-    var bt_fem = document.getElementById('pag_fem')
 
     var section_aces = document.getElementById('section_aces')
     var bt_aces = document.getElementById('pag_aces')
@@ -38,19 +37,80 @@
     var secrion_skt = document.getElementById('section_skt')
     var bt_skt = document.getElementById('pag_skt')
 
-    bt_masc.addEventListener("click", function() {
-       
-        var section_masc = document.getElementById('section_masc') 
-        var bt_masc = document.getElementById('pag_masc')
+    /*bt_masc.addEventListener("click", function() {
+       ;
+        
 
         if(section_masc.style.display ==="none"){
             section_masc.style.display = "block";
         } else {
             section_masc.style.display ="none"
         }
+    */
+
+    var bt_masc = document.getElementById('pag_masc')
+       
+        bt_masc.addEventListener("click", function(){
+            var section_masc = document.getElementById('section_masc');
+           
+            section_fem.remove();
+            section_aces.remove();  
+            secrion_skt.remove(); 
+
+
+            section_masc.classList.toggle("hide");        
         
+        
+        })
+
     
-    
-    
-    
-    })
+
+
+
+   
+    var bt_fem = document.getElementById('pag_fem')
+        bt_fem.addEventListener("click", function(){
+            var section_fem = document.getElementById('section_fem')   
+            
+
+
+
+
+            section_masc.classList.toggle("hide")
+        
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
