a099bfb4a90a1e569e04703469a078dfed9ae6fe
00live <128560704+00live@users.noreply.github.com>
2023-04-24 22:18:32 -0300

    .

--- produtos_gerais/masc/Item1/script.js
+++ produtos_gerais/masc/Item1/script.js
@@ -1,3 +1,23 @@
+var radio = document.querySelector('.manual_bt')
+var cont = 1
+
+document.getElementById('rd1').checked = true
+
+    function proxImg (){
+       cont++ 
+       
+       if(cont>4){
+       cont = 1}
+
+       document.getElementById('rd'+cont).checked = true
+
+    }
+
+
+
+    setInterval(() => {
+        proxImg ()
+    }, 5000)
 
 
 
